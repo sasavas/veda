e8f033cd6985060abf85212404a2ad3b0389c19d
savas <savasusame@gmail.com>
2023-12-29 15:16:42 +0300

    refactor: override Equals operator in Entity.cs

--- Veda.Domain/Abstract/Entity.cs
+++ Veda.Domain/Abstract/Entity.cs
@@ -2,5 +2,35 @@ namespace Veda.Application.Abstract;
 
 public abstract class Entity
 {
-    public int Id { get; set; }
+    public int Id { get; private set; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        if (obj is Entity entity)
+        {
+            return entity.Id == this.Id;
+        }
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+
+    public static bool operator ==(Entity left, Entity right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(Entity left, Entity right)
+    {
+        return !(left == right);
+    }
 }
