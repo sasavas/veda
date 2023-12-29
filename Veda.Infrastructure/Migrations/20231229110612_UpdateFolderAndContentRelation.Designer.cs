ff228ee4d974c695e2c6202f805bdbafd5281d0b
savas <savasusame@gmail.com>
2023-12-29 15:21:27 +0300

    apply database migrations

--- Veda.Infrastructure/Migrations/20231229110612_UpdateFolderAndContentRelation.Designer.cs
+++ Veda.Infrastructure/Migrations/20231229110612_UpdateFolderAndContentRelation.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Veda.Infrastructure.DataAccess;
@@ -11,9 +12,11 @@ using Veda.Infrastructure.DataAccess;
 namespace Veda.Infrastructure.Migrations
 {
     [DbContext(typeof(VedaDbContext))]
-    partial class VedaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231229110612_UpdateFolderAndContentRelation")]
+    partial class UpdateFolderAndContentRelation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -143,7 +146,7 @@ namespace Veda.Infrastructure.Migrations
 
                     NpgsqlPropertyBuilderExtensions.UseIdentityByDefaultColumn(b.Property<int>("Id"));
 
-                    b.Property<DateTime?>("DeletionDate")
+                    b.Property<DateTime>("DeletionDate")
                         .HasColumnType("timestamp with time zone")
                         .HasColumnName("deletion_date");
 
---
+++ Veda.Infrastructure/Migrations/20231229110612_UpdateFolderAndContentRelation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Veda.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateFolderAndContentRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_digital_content_folder_folder_id",
+                table: "digital_content");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "deletion_date",
+                table: "digital_content",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "upload_date",
+                table: "digital_content",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_digital_content_folder_folder_id",
+                table: "digital_content",
+                column: "folder_id",
+                principalTable: "folder",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_digital_content_folder_folder_id",
+                table: "digital_content");
+
+            migrationBuilder.DropColumn(
+                name: "deletion_date",
+                table: "digital_content");
+
+            migrationBuilder.DropColumn(
+                name: "upload_date",
+                table: "digital_content");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_digital_content_folder_folder_id",
+                table: "digital_content",
+                column: "folder_id",
+                principalTable: "folder",
+                principalColumn: "id");
+        }
+    }
+}
